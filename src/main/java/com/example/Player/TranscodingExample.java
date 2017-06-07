5ce77332c54a110106f382d54ca2b1cc849d0413
Anant <anant.shripanthri@gmail.com>
2017-06-07 15:21:17 -0500

    ,

--- src/main/java/com/example/Player/TranscodingExample.java
+++ src/main/java/com/example/Player/TranscodingExample.java
@@ -9,14 +9,22 @@ public class TranscodingExample {
 
 	
 	 public static void main(String[] args) throws IOException {
+		// String cmd="C:/ffmpeg/ffmpeg.exe -i test_m.mov test_m1234.mp4";
+		 String cmd[] = {"C:\\ffmpeg\\bin\\ffmpeg","-i","C:\\Users\\anpanthri\\Desktop\\vids\\test_m.mov","C:\\Users\\anpanthri\\Desktop\\vids\\testinglk.mp4"};
 		 File wd = new File("C:/Users/anpanthri/Desktop/vids");
-		 Process p=Runtime.getRuntime().exec("c:/ffmpeg/ -i c:/ffmpeg/bin/test_m.mov -codec:v libx264 -vf scale=480:-1 -y c:/ffmpeg/bin/output.mp4");/*;c:\ffmpeg\ -i c:\ffmpeg\bin\input.mp4 -codec:v libx264 -vf scale=480:-1 -y c:\ffmpeg\bin\output.mp4*/
-		 BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		 String line ="";
-		 while((line=br.readLine())!=null)
-			 System.out.println(line);
+		  String[] cmdArray = new String[2];
+	      cmdArray[0] = "ffmpeg";
+	     cmdArray[1] = "example.txt";
+	    Long stime=System.nanoTime();
 		 
+		 Process p=Runtime.getRuntime().exec(" C:\\ffmpeg\\bin\\ffmpeg -i C:\\Users\\anpanthri\\Desktop\\vids\\final1.mp4 C:\\Users\\anpanthri\\Desktop\\vids\\reap.mov");//c:\ffmpeg\ -i c:\ffmpeg\bin\input.mp4 -codec:v libx264 -vf scale=480:-1 -y c:\ffmpeg\bin\output.mp4
+		 /*BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		 while( br.readLine() !=null)
+			 System.out.println(br.readLine());
+		 Long etime=System.nanoTime();
+		 System.out.println(stime-etime);*/
 	 }
+	 
 
 	}
 
